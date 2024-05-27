a7cfea72165ab8a29a72a625f556487000dd8c9c
Zane <git@formulaic.cloud>
2024-05-26 22:34:04 -0700

    password hashing wowow

--- src/rember/auth.clj
+++ src/rember/auth.clj
@@ -4,29 +4,28 @@
    [ring.util.response :refer [response]]
    ; database
    [toucan2.core :as t2]
-   [methodical.core :as m]
    ; password hashing
-   [caesium.crypto.pwhash :as pwhash]
-   [cheshire.core :as cc]))
-
+   [caesium.crypto.pwhash :as cs]))
 
 (defn create-session [])
 
-(defn login [req]
+(defn login [_req]
   {:status 200 :body "login endpoint" :headers {}})
 
-(defn create-account [username]
-  (t2/insert! :models/users :username username))
+(defn create-account [username pass]
+  (let [further-hash (cs/pwhash-str pass cs/opslimit-interactive cs/memlimit-interactive)]
+    (t2/insert! :models/users :username username :pwhash further-hash)))
 
 ; https://github.com/ring-clojure/ring/wiki/Sessions
 ; TODO: password? :P
 (defn register-handler [req]
-  (let [username (get-in req [:json :username])]
+  (let [{:keys [username pass]} (:json req)
+        valid-req (not (or (nil? username) (nil? pass)))]
     (cond
-      (nil? username) {:status 400 :body "no username provided" :headers {}}
+      (not valid-req) {:status 400 :body "bad request" :headers {}}
       (t2/exists? :models/users :username username) {:status 400 :body "username taken" :headers {}}
       :else (do
-              (create-account username)
+              (create-account username pass)
               (->
                (response "ok")
                (assoc :session {:username username}))))))
