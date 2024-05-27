3b4572aaf94aa27c17b596c684bac76213a97f24
Zane <git@formulaic.cloud>
2024-05-27 15:07:45 -0700

    stored entry recall is now functional

--- src/rember/storage.clj
+++ src/rember/storage.clj
@@ -1,7 +1,16 @@
 (ns rember.storage
-  (:require [ring.util.response :refer [response]]))
+  (:require [ring.util.response :refer [response]]
+            [toucan2.core :as t2]
+            [cheshire.core :as cc]))
+
+(t2/define-named-query ::user-entries
+  {:select [:userdata.entry] :from [:userdata] :join [:users [:= :users.id :userdata.userid]]})
 
 (defn list-stored [req]
   (if-let [username (get-in req [:session :username])]
-    (response (format "list stored route w/ username: %s", username))
+    (let [entries (t2/select :models/userdata :users.username username ::user-entries)]
+      (->> entries
+        (map :entry)
+        cc/generate-string
+        response))
     {:status 401 :body "unauthorized" :headers {}}))
