52bb3cddc5efa8f29104c867d03baba2fc6a2b83
Zane <git@formulaic.cloud>
2024-05-26 22:55:29 -0700

    logging in works now

--- src/rember/auth.clj
+++ src/rember/auth.clj
@@ -5,19 +5,24 @@
    ; database
    [toucan2.core :as t2]
    ; password hashing
-   [caesium.crypto.pwhash :as cs]))
+   [caesium.crypto.pwhash :as pwhash]))
 
-(defn create-session [])
-
-(defn login [_req]
-  {:status 200 :body "login endpoint" :headers {}})
+(defn login [req]
+  (let [{:keys [username pass]} (:json req)
+        valid-req (not (or (nil? username) (nil? pass)))]
+    (if
+     (not valid-req) {:status 400 :body "bad request" :headers {}}
+     (let [{pwhash :pwhash} (t2/select-one :models/users :username username)]
+       (if (or (nil? pwhash) (not= (pwhash/pwhash-str-verify pwhash pass) 0))
+         {:status 400 :body "login failed"}
+         (->
+          (response "ok")
+          (assoc :session {:username username})))))))
 
 (defn create-account [username pass]
-  (let [further-hash (cs/pwhash-str pass cs/opslimit-interactive cs/memlimit-interactive)]
+  (let [further-hash (pwhash/pwhash-str pass pwhash/opslimit-interactive pwhash/memlimit-interactive)]
     (t2/insert! :models/users :username username :pwhash further-hash)))
 
-; https://github.com/ring-clojure/ring/wiki/Sessions
-; TODO: password? :P
 (defn register-handler [req]
   (let [{:keys [username pass]} (:json req)
         valid-req (not (or (nil? username) (nil? pass)))]
@@ -29,6 +34,3 @@
               (->
                (response "ok")
                (assoc :session {:username username}))))))
-
-; (defn verify-pwhash [hash]
-;   (pwhash/pwhash-str-verify))
